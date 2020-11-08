6f7e3b6c68935a35f7ffc7ae8f25d4ad401ff1f3
sudachi0114 <jnevariste@gmail.com>
2020-11-09 01:42:26 +0900

    不要な行を削除

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,13 +5,11 @@ object Main extends App {
   println("契約書だよ。そこに名前を書きな。")
 
   val scanner = new Scanner(System.in)
-  scanner.useDelimiter("\n")
   val name: String = scanner.nextLine()
 
   printf("フン。%sというのかい。贅沢な名だねェ。\n", name)
 
   val nameVector = name.toVector
-  println(nameVector.length, nameVector(0))
 
   val newName = nameVector(Random.nextInt(name.length))
   printf("今からお前の名前は%sだ。いいかい、%sだよ。わかったら返事をするんだ、%s!!\n", newName, newName, newName)
